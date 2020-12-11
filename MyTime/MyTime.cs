ff7ef158e653cbb2020f92b3aaaba61c00b1b2da
Sasha <sashapysmenny@gmail.com>
2020-12-11 17:27:12 +0200

    init

---
+++ MyTime/MyTime.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace MyTime
+{
+    class MyTime
+    {
+        private int hour, minute, second;
+        public MyTime(int hour_, int minute_, int second_)
+        {
+            hour = hour_;
+            minute = minute_;
+            second = second_;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(hour + ":" + minute.ToString("00") + ":" + second.ToString("00"));
+        }
+
+        public void TimeFormat()
+        {
+            int t = TimeSinceMidnight();
+            int secPerDay = 60 * 60 * 24;
+            t = ((t % secPerDay) + secPerDay) % secPerDay;
+            hour = t / 3600;
+            minute = (t / 60) % 60;
+            second = t % 60;
+        }
+
+        public int TimeSinceMidnight()
+        {
+            return hour * 3600 + minute * 60 + second;
+        }
+
+        public MyTime TimeSinceMidnight(int t)
+        {
+            hour = t / 3600;
+            minute = (t / 60) % 60;
+            second = t % 60;
+            TimeFormat();
+            return new MyTime(hour, minute, second);
+        }
+
+
+        public MyTime AddOneSecond()
+        {
+            second++;
+            TimeFormat();
+            return new MyTime(hour, minute, second);
+        }
+        public MyTime AddOneMinute()
+        {
+            minute++;
+            TimeFormat();
+            return new MyTime(hour, minute, second);
+        }
+        public MyTime AddOneHour()
+        {
+            hour++;
+            TimeFormat();
+            return new MyTime(hour, minute, second);
+        }
+
+        public MyTime AddSeconds(int t)
+        {
+            second += t;
+            int timeInSeconds = hour * 3600 + minute * 60 + second;
+            return TimeSinceMidnight(timeInSeconds);
+        }
+
+        public int Difference(MyTime mt1)
+        {
+            return (TimeSinceMidnight() - mt1.TimeSinceMidnight());
+        }
+
+        public string WhatLesson()
+        {
+            int firstLesson = new MyTime(8, 0, 0).TimeSinceMidnight();
+            int lastLesson = new MyTime(17, 40, 0).TimeSinceMidnight();
+            int break_ = new MyTime(0, 20, 0).TimeSinceMidnight();
+            int lessonInSecond = new MyTime(0, 80, 0).TimeSinceMidnight();
+            int setTime = TimeSinceMidnight();
+            int lesson = 0;
+            if (hour < 8)
+            {
+                return "пари ще не почалися";
+            }
+            else
+            {
+                for (int i = firstLesson; i < lastLesson; i += lessonInSecond + break_)
+                {
+                    lesson++;
+                    if ((setTime > i + lessonInSecond && setTime < i + lessonInSecond + break_) && setTime <= lastLesson)
+                    {
+                        return "перерва мiж " + lesson + "-ю та " + (lesson + 1) + "-ю парами";
+                    }
+                    else if (setTime >= i && setTime <= i + lessonInSecond)
+                    {
+                        return lesson + "-а пара";
+                    }
+                }
+                return "пари вже скiнчилися";
+            }
+        }
+    }
+}
---
+++ MyTime/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hour = ");
+            int h = int.Parse(Console.ReadLine());
+            Console.Write("Minute = ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Second = ");
+            int s = int.Parse(Console.ReadLine());
+            MyTime myTime = new MyTime(h, m, s);
+            myTime.TimeFormat();
+            Console.WriteLine("Time - " + myTime);
+            int t = myTime.TimeSinceMidnight();
+            Console.WriteLine("Seconds after midnight - " + t);
+            Console.WriteLine("Mytime format - " + myTime.TimeSinceMidnight(t));
+            myTime.AddOneSecond();
+            Console.WriteLine("Add one second - " + myTime);
+            myTime.AddOneMinute();
+            Console.WriteLine("Add one minute - " + myTime);
+            myTime.AddOneHour();
+            Console.WriteLine("Add one hour - " + myTime);
+            Console.Write("Enter seconds = ");
+            int addSeconds = int.Parse(Console.ReadLine());
+            Console.WriteLine("Added seconds - " + myTime.AddSeconds(addSeconds));
+            Random rand = new Random();
+            MyTime newTime = new MyTime(rand.Next(0, 24), rand.Next(0, 60), rand.Next(0, 60));
+            Console.WriteLine("Difference between " + myTime + " and " + newTime + " is " + myTime.Difference(newTime) + " sec");
+            Console.WriteLine(myTime.WhatLesson());
+            Console.ReadKey();
+        }
+    }
+}
